6789e42003322ea45c9f45a19d0deb5626bbcb1f
c7tt8nt2p <chantapat.ct@gmail.com>
2019-06-16 15:40:59 +0700

    finish the calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Product} from './model/product';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Title} from '@angular/platform-browser';
+import {KeyValue} from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -18,6 +19,12 @@ export class AppComponent implements OnInit {
     totalPrice: number = 0;
     totalPriceInitialDiscountDiff: number = 0;
 
+    possibleGroupMap: Map<number, Product[]> = new Map<number, Product[]>();
+    impossibleGroupMap: Map<number, Product[]> = new Map<number, Product[]>();
+
+    keyDescOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+        return a.key > b.key ? -1 : (b.key > a.key ? 1 : 0);
+    };
 
     constructor(private titleService: Title) {
     }
@@ -27,8 +34,8 @@ export class AppComponent implements OnInit {
         this.titleService.setTitle(this.title);
         this.productForm = new FormGroup({
             initialDiscount: new FormControl(+0, [Validators.required, Validators.pattern('\\d{1,2}(?!\\d)|100')]),
-            promotionPrice: new FormControl(+0, [Validators.required, Validators.pattern('\\d{1,2}(?!\\d)|100')]),
-            promotionDiscount: new FormControl(+0, [Validators.required, Validators.pattern('\\d{1,2}(?!\\d)|100')])
+            promotionPrice: new FormControl(+0, [Validators.required, Validators.pattern('^[0-9]*$')]),
+            promotionDiscount: new FormControl(+0, [Validators.required, Validators.pattern('^[0-9]*$')])
         });
     }
 
@@ -57,6 +64,10 @@ export class AppComponent implements OnInit {
     }
 
     validate(): void {
+        this.initialDiscount.patchValue(+this.initialDiscount.value);
+        this.promotionPrice.patchValue(+this.promotionPrice.value);
+        this.promotionDiscount.patchValue(+this.promotionDiscount.value);
+
         for (let aProduct of this.productList) {
             let name = aProduct.name;
             if (!name) {
@@ -88,20 +99,81 @@ export class AppComponent implements OnInit {
 
         // total price and initial discount difference
         let initialDiscount = +this.initialDiscount.value;
-        this.totalPriceInitialDiscountDiff = this.totalPrice - (this.totalPrice * initialDiscount / 100);
+        this.totalPriceInitialDiscountDiff = this.takeInitialDiscount(this.totalPrice)
     }
 
     calculate(): void {
         this.validate();
+        this.possibleGroupMap.clear();
+        this.impossibleGroupMap.clear();
         if (this.valid) {
             this.doGrouping();
         }
     }
 
     doGrouping(): void {
-        for (let i = 0; i < this.productList.length; i++) {
-            let aProduct = this.productList[i];
+        let groupPriceMap: Map<number, Product[]> = new Map<number, Product[]>();
+
+        let combinationList: Product[] = this.getCombinations(this.productList);
+        for (let i = 0; i < combinationList.length; i++) {
+            let aCombination: Product[] = JSON.parse(JSON.stringify(combinationList[i]));
+            if (aCombination.length == 0) {
+                continue;
+            }
+            let totalPrice = aCombination.map(p => p.price).reduce((sum, current) => sum + current, 0);
+            groupPriceMap.set(totalPrice, aCombination);
         }
+        let sorted = AppComponent.sortGroupPriceMap(groupPriceMap);
+        this.displayResults(sorted);
+    }
+
+    displayResults(sortedGroupPriceMap: Map<number, Product[]>): void {
+        sortedGroupPriceMap.forEach((productList, totalPrice) => {
+            let priceWithInitialDiscount = this.takeInitialDiscount(totalPrice);
+            console.log(priceWithInitialDiscount + ' :: ' + this.promotionPrice.value + ' --> ' + (totalPrice >= this.promotionPrice.value));
+            if (priceWithInitialDiscount >= this.promotionPrice.value) {
+                this.possibleGroupMap.set(totalPrice, productList);
+            } else {
+                this.impossibleGroupMap.set(totalPrice, productList);
+            }
+        });
+    }
+
+    static sortGroupPriceMap(toBeSorted: Map<number, Product[]>): Map<number, Product[]> {
+        let sortedMap: Map<number, Product[]>;
+
+        sortedMap = new Map(
+            Array
+                .from(toBeSorted)
+                .sort((a, b) => {
+                    return b[0] - a[0];
+                })
+        );
+
+        return sortedMap;
+    }
+
+    getCombinations(array): Product[] {
+        function fork(i, t) {
+            if (i === array.length) {
+                result.push(t);
+                return;
+            }
+            fork(i + 1, t.concat([array[i]]));
+            fork(i + 1, t);
+        }
+
+        let result: Product[] = [];
+        fork(0, []);
+        return result;
+    }
+
+    productListToText(productList: Product[]): string {
+        return productList.map(p => p.name).join(', ');
+    }
+
+    takeInitialDiscount(originalPrice: number): number {
+        return originalPrice - (originalPrice * this.initialDiscount.value / 100);
     }
 
     get initialDiscount() {
